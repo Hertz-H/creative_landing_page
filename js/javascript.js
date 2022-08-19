e394c10214debf93428d0418c9f9474a439b9181
Hertz-H <75604688+Hertz-H@users.noreply.github.com>
2022-08-19 16:53:48 +0300

    inspirationTemplate

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function stretch(){
+	var settingBox= document.getElementById("setting-box");
+	var settingicon= document.getElementById("setting-icon");
+	settingBox.style.color="red";
+	settingicon.style.color="green";
+	
+	
+}
